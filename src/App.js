0a67ec2626258d3e124d01028ae5f3619a18df8b
Mrunal Deo <mrunaldeo16@gmail.com>
2024-03-30 22:05:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Discover from './components/Discover';
+import Items from './components/Items';
+import Products from './components/Products';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Discover />
+      <Items />
+      <Products />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
